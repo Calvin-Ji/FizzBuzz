b2e3527f40a598cc431a2780efbbf4274e2689f7
calvinji <calvinchenji@gmail.com>
2024-09-09 21:40:42 -0400

    created multiples and main files and made the doFizzBuzz function private

---
+++ src/Multiples.java
@@ -0,0 +1,9 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int multiplesof3 = 1000/3;
+        int multiplesof5 = 1000/5;
+        int multiplesof15 = 1000/15;
+        System.out.println(multiplesof3 + multiplesof5 - multiplesof15);
+    }
+
+}
